dd01d4347f208c0f14410e78d4f11bad74944613
George Yu <georgeyu26@gmail.com>
2020-10-10 19:30:41 -0400

    Added solar gradients and gestures

--- Elemental/ViewController.swift
+++ Elemental/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var conditionLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
-    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var backgroundImage: UIImageView!
     
     let gradientLayer = CAGradientLayer()
     let apiKey = "8c1e240150949fb7bfe0bf0503c8a20e"
@@ -28,10 +28,28 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     var lon = -80.5204
     var activityIndicator: NVActivityIndicatorView!
     let locationManager = CLLocationManager()
+    var imageIndex = 0
+    let backgroundImages: [UIImage] = [
+        UIImage(named: "1.png")!,
+        UIImage(named: "2.png")!,
+        UIImage(named: "3.png")!,
+        UIImage(named: "4.png")!,
+        UIImage(named: "5.png")!,
+        UIImage(named: "6.png")!,
+        UIImage(named: "7.png")!,
+        UIImage(named: "8.png")!,
+        UIImage(named: "9.png")!,
+        UIImage(named: "10.png")!,
+        UIImage(named: "11.png")!,
+        UIImage(named: "12.png")!,
+        UIImage(named: "13.png")!,
+        UIImage(named: "14.png")!,
+        UIImage(named: "15.png")!,
+        UIImage(named: "16.png")!,
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        backgroundView.layer.addSublayer(gradientLayer)
         let indicatorSize: CGFloat = 70
         let indicatorFrame = CGRect(x: (view.frame.width-indicatorSize)/2, y: (view.frame.height-indicatorSize)/2, width: indicatorSize, height: indicatorSize)
         activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color: UIColor.white, padding: 20.0)
@@ -44,17 +62,49 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.desiredAccuracy = kCLLocationAccuracyBest
             locationManager.startUpdatingLocation()
         }
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.view.addGestureRecognizer(tap)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeLeft.direction = .left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeUp.direction = .up
+        self.view.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeDown.direction = .down
+        self.view.addGestureRecognizer(swipeDown)
+        
+    }
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
+        setBackgroundImage()
+    }
+    
+    @objc func handleGesture(gesture: UISwipeGestureRecognizer) -> Void {
+       if gesture.direction == UISwipeGestureRecognizer.Direction.right {
+            prevBackgroundImage()
+       } else if gesture.direction == UISwipeGestureRecognizer.Direction.left {
+            nextBackgroundImage()
+       }
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        setDayGradientBackground()
+        
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[0]
         lat = location.coordinate.latitude
         lon = location.coordinate.longitude
-        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&units=metric").responseJSON {
+        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(apiKey)&units=metric").responseJSON { [self]
             response in
             self.activityIndicator.stopAnimating()
             if let responseStr = response.result.value {
@@ -71,35 +121,103 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 let date = Date()
                 let dateFormatter = DateFormatter()
                 dateFormatter.dateFormat = "EEEE"
-                self.dayLabel.text = dateFormatter.string(from: date)
+                let currentDateTime = Date()
+                let formatter = DateFormatter()
+                formatter.timeStyle = .none
+                formatter.dateStyle = .long
+                var dateString = formatter.string(from: currentDateTime)
+                dateString = String(dateString.dropLast(6))
+                self.dayLabel.text = dateFormatter.string(from: date) + ", " + dateString
+                setBackgroundImage()
                 
-                let suffix = iconName.suffix(1)
-                if (suffix == "n") {
-                    self.setNightGradientBackground()
-                } else {
-                    self.setDayGradientBackground()
-                }
+                
             }
         }
         self.locationManager.stopUpdatingLocation()
     }
     
+    func setBackgroundImage() {
+        let date = Date()
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        if (0 <= hour && hour <= 3) {
+            setBackgroundImage(imageIndex: 0)
+            imageIndex = 0
+        } else if (hour == 4) {
+            setBackgroundImage(imageIndex: 1)
+            imageIndex = 1
+        } else if (hour == 5) {
+            setBackgroundImage(imageIndex: 2)
+            imageIndex = 2
+        } else if (hour == 6) {
+            setBackgroundImage(imageIndex: 3)
+            imageIndex = 3
+        } else if (hour == 7) {
+            setBackgroundImage(imageIndex: 4)
+            imageIndex = 4
+        } else if (hour == 8) {
+            setBackgroundImage(imageIndex: 5)
+            imageIndex = 5
+        } else if (hour == 9) {
+            setBackgroundImage(imageIndex: 6)
+            imageIndex = 6
+        } else if (hour == 10) {
+            setBackgroundImage(imageIndex: 7)
+            imageIndex = 7
+        } else if (11 <= hour && hour <= 15) {
+            setBackgroundImage(imageIndex: 8)
+            imageIndex = 8
+        } else if (hour == 16) {
+            setBackgroundImage(imageIndex: 9)
+            imageIndex = 9
+        } else if (hour == 17) {
+            setBackgroundImage(imageIndex: 10)
+            imageIndex = 10
+        } else if (hour == 18) {
+            setBackgroundImage(imageIndex: 11)
+            imageIndex = 11
+        } else if (hour == 19) {
+            setBackgroundImage(imageIndex: 12)
+            imageIndex = 12
+        } else if (hour == 20) {
+            setBackgroundImage(imageIndex: 13)
+            imageIndex = 13
+        } else if (hour == 21) {
+            setBackgroundImage(imageIndex: 14)
+            imageIndex = 14
+        } else if (hour == 22) {
+            setBackgroundImage(imageIndex: 15)
+            imageIndex = 15
+        } else if (hour == 23) {
+            setBackgroundImage(imageIndex: 0)
+            imageIndex = 0
+        }
+    }
+    
+    func setBackgroundImage(imageIndex: Int) {
+        let toImage = backgroundImages[imageIndex]
+        UIView.transition(with: self.backgroundImage, duration: 0.5, options: .transitionCrossDissolve, animations: {self.backgroundImage.image = toImage}, completion: nil)
+    }
+    
+    func nextBackgroundImage() {
+        if (imageIndex == 15) {
+            imageIndex = 0
+        } else {
+            imageIndex += 1
+        }
+        setBackgroundImage(imageIndex: imageIndex)
+    }
+    func prevBackgroundImage() {
+        if (imageIndex == 0) {
+            imageIndex = 15
+        } else {
+            imageIndex -= 1
+        }
+        setBackgroundImage(imageIndex: imageIndex)
+    }
+    
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         
-    }
-    
-    func setDayGradientBackground() {
-        let topColor = UIColor(red: 31.0/255.0, green: 139.0/255.0, blue: 186.0/255.0, alpha: 1.0).cgColor
-        let bottomColor = UIColor(red: 96.0/255.0, green: 167.0/255.0, blue: 204.0/255.0, alpha: 1.0).cgColor
-        gradientLayer.frame = view.bounds
-        gradientLayer.colors = [topColor, bottomColor]
-    }
-    
-    func setNightGradientBackground() {
-        let topColor = UIColor(red: 0.0/255.0, green: 5.0/255.0, blue: 27.0/255.0, alpha: 1.0).cgColor
-        let bottomColor = UIColor(red: 49.0/255.0, green: 57.0/255.0, blue: 85.0/255.0, alpha: 1.0).cgColor
-        gradientLayer.frame = view.bounds
-        gradientLayer.colors = [topColor, bottomColor]
     }
     
 }
